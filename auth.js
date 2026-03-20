c7071362e8ff0152fa19b6787f0d5676c3dad431
Devaki <devaki2630@gmail.com>
2026-03-20 20:33:31 +0530

    Update email transporter configuration for Gmail

--- auth.js
+++ auth.js
@@ -19,7 +19,9 @@ function generateOTP() {
 // Email transporter
 function getTransporter() {
   return nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
